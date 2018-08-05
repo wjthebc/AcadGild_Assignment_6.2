e72da53938942ff3588e42ada5e766d8988c1360
wjthebc <40471359+wjthebc@users.noreply.github.com>
2018-08-05 16:55:35 -0400

    Add files via upload

---
+++ Session 6 Assignment 2/script.js
@@ -0,0 +1,25 @@
+/*defines variables*/
+var recipeName = 'Creamy squash linguine';
+var preparationTime = '45';
+var recipeBy = 'Jimmy Cutter';
+var isVegetarian = 'true';
+var calories = '223.7';
+/*logs variable values to console*/
+console.log ('Recipe details');
+console.log ('----------');
+console.log ('Recipe Name:' + recipeName);
+console.log ('Preparation Time:' + preparationTime);
+console.log ('Recipe By:' + recipeBy);
+console.log ('is Vegetarian Dish:' + isVegetarian);
+console.log ('Calories:' + calories);
+/*redefines variables*/
+preparationTime = '60'
+recipeBy = 'Jimmy Cutter George'
+/*re-logs variables to console*/
+console.log ('Recipe details after update')
+console.log ('----------');
+console.log ('Recipe Name:' + recipeName);
+console.log ('Preparation Time:' + preparationTime);
+console.log ('Recipe By:' + recipeBy);
+console.log ('is Vegetarian Dish:' + isVegetarian);
+console.log ('Calories:' + calories);
